25c61baa7ee990f4f77d92ac1b25d4aa77b2b5b6
Ukd1796 <ujjwal.kumar@somaiya.edu>
2022-08-07 13:06:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
-import logo from "./logo.svg";
-import {
-  TextField,
-  Grid,
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-  IconButton,
-  Button,
-} from "@material-ui/core";
-import './App.css'
-import MenuIcon from "@material-ui/icons/Menu";
+import { Grid, Box, Button } from "@material-ui/core";
+import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import * as tf from "@tensorflow/tfjs";
-// import {loadGraphModel} from '@tensorflow/tfjs-converter';
-
-// import * as posenet from '@tensorflow-models/posenet';
 import Webcam from "react-webcam";
-import { createWorker,createScheduler  } from 'tesseract.js';
-// import * as cvstfjs from '@microsoft/customvision-tfjs';
-
 
 function App() {
   const useStyles = makeStyles((theme) => ({
@@ -37,18 +20,14 @@ function App() {
   }));
   const classes = useStyles();
 
-  
   const webcamRef = React.useRef(null);
 
   const [videoWidth, setVideoWidth] = useState(960);
   const [videoHeight, setVideoHeight] = useState(640);
 
-
   const [model, setModel] = useState();
 
- 
-
-  
+  // loading the pre-trained model
   async function loadModel() {
     try {
       const model = await cocoSsd.load();
@@ -60,20 +39,18 @@ function App() {
     }
   }
 
-  
   useEffect(() => {
     tf.ready().then(() => {
       loadModel();
     });
   }, []);
 
-
-  async function predictionFunction() {
+  async function predictFunction() {
     const predictions = await model.detect(document.getElementById("img"));
     // setVideoHeight(webcamRef.current.video.videoHeight);
     // setVideoWidth(webcamRef.current.video.videoWidth);
     var cnvs = document.getElementById("myCanvas");
-    cnvs.width =  webcamRef.current.video.videoWidth;
+    cnvs.width = webcamRef.current.video.videoWidth;
     cnvs.height = webcamRef.current.video.videoHeight;
     // cnvs.style.position = "absolute";
 
@@ -95,8 +72,7 @@ function App() {
           let bboxLeft = predictions[n].bbox[0];
           let bboxTop = predictions[n].bbox[1];
           let bboxWidth = predictions[n].bbox[2];
-          let bboxHeight = predictions[n].bbox[3]; // - bboxTop;
-
+          let bboxHeight = predictions[n].bbox[3];
           console.log("bboxLeft: " + bboxLeft);
           console.log("bboxTop: " + bboxTop);
 
@@ -122,69 +98,28 @@ function App() {
 
           ctx.lineWidth = 3;
           ctx.stroke();
-
-          console.log("detected");
         }
       }
     }
 
-    setTimeout(() => predictionFunction(), 500);
+    setTimeout(() => predictFunction(), 100);
   }
 
-
-  // useEffect(() => {
-  //   //prevent initial triggering
-  //   if (mounted.current) {
-  //     predictionFunction();
-   
-  //   } else {
-  //     mounted.current = true;
-  //   }
-  // }, [start]);
-
-
-
- 
   const videoConstraints = {
     height: 1080,
     width: 1920,
-    maxWidth: "100vw",
+    maxWidth: "50vw",
     facingMode: "environment",
   };
-
-
-   
- 
-
-
 
   return (
     <div
       style={{
         width: "100%",
         height: "100%",
-        marginTop: -8,
-        backgroundImage:
-          "radial-gradient( circle 993px at 0.5% 50.5%,  rgba(137,171,245,0.37) 0%, rgba(245,247,252,1) 100.2% )",
+        marginTop: -3,
       }}
     >
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Object Detection
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-      <Box mt={1} />
       <Grid
         container
         style={{
@@ -193,7 +128,7 @@ function App() {
           alignItems: "center",
           justifyContent: "center",
           display: "flex",
-          padding: 20,
+          padding: 10,
         }}
       >
         <Grid
@@ -217,17 +152,19 @@ function App() {
                   backgroundColor: "blueviolet",
                   width: "50%",
                   maxWidth: "250px",
+                  position: "relative",
+                  bottom: "325px",
                 }}
                 onClick={() => {
-                  predictionFunction();
+                  predictFunction();
                 }}
               >
-                Start Detect
+                Start Detecting
               </Button>
             }
             <Box mt={2} />{" "}
           </>
-          <div style={{ position: "absolute", top: "400px", zIndex: "9999" }}>
+          <div style={{ position: "absolute", top: "80px", zIndex: "1" }}>
             <canvas
               id="myCanvas"
               width={videoWidth}
@@ -235,32 +172,17 @@ function App() {
               style={{ backgroundColor: "transparent" }}
             />
           </div>
-          <div style={{ position: "absolute", top: "400px" }}>
-            {/* <img
-          style={{ width: videoWidth, objectFit: "fill" }}
-          id="img"
-          src={imageData}
-        ></img>   <Webcam
-        audio={false}
-        id="img2"
-        ref={webcamRef}
-        // width={640}
-        screenshotQuality={1}
-        screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
-      /> */}
+          <div style={{ position: "absolute", top: "80px" }}>
             <Webcam
               audio={false}
               id="img"
               ref={webcamRef}
-              // width={640}
               screenshotQuality={1}
               screenshotFormat="image/jpeg"
               videoConstraints={videoConstraints}
             />
           </div>
         </Grid>
-        <Grid item xs={12} md={12}></Grid>
       </Grid>
     </div>
   );
